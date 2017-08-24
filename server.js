49e4ac0cbc83a5648d47b79fc9177d84e2bf3d6e
DFG <cocaine31197@gmail.com>
2017-08-24 13:08:03 +0530

    Finished

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ var app = express();
 
 
 var port = 8888;
+insta.use({
+// get access token here: http://instagram.pixelunion.net/
+access_token: "2201840928.1677ed0.7798ce602d98412ab34a731fc302b2c9",
+});
 
 app.use(express.static(__dirname + "/public"));
 app.set("view engine","ejs");
 
 app.get("/",(req,res)=>{
-res.render("pages/index");
+insta.user_self_media_recent((err,medias,pagination,remaining,limit)=>{
+res.render("pages/index",{grams: medias});
+});
 });
 
 
